20de6b25b91f632e51ef8ec13d9c77c7bdaa7c47
Hanz Felix Tumulak <tumulakhanz@gmail.com>
2025-06-27 20:54:40 +0800

    style: apply prettier formatting

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,7 +15,7 @@ export default function Navbar() {
   return (
     <div className="fixed bottom-0 w-full text-sm">
       <div className="container mx-auto flex justify-end md:px-4">
-        <div className="p-3 pb-2 flex flex-col items-stretch gap-4 bg-cmono-25 min-w-full md:min-w-1/3 lg:min-w-1/4">
+        <div className="bg-cmono-25 flex min-w-full flex-col items-stretch gap-4 p-3 pb-2 md:min-w-1/3 lg:min-w-1/4">
           <div className="flex justify-between">
             <div>
               <RiArrowLeftDoubleLine className="fill-cmono-50" />
@@ -44,6 +44,7 @@ export default function Navbar() {
               width={36}
               height={36}
               alt="user avatar"
+              className="basis-9"
               src="https://placehold.co/36x36"
             />
           </div>
