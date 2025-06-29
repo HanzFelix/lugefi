344d55e3075ecc84f5dcf44d592ac3afff8afe60
Hanz Felix Tumulak <tumulakhanz@gmail.com>
2025-06-29 20:55:22 +0800

    chore: add db config for local development

--- src/db/database.ts
+++ src/db/database.ts
@@ -1,9 +1,25 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon, neonConfig } from "@neondatabase/serverless";
+let connectionString = process.env.DATABASE_URL!;
 
+// Configuring Neon for local development
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === "development") {
+  connectionString = "postgres://postgres:postgres@db.localtest.me:5432/main";
+  neonConfig.fetchEndpoint = (host) => {
+    const [protocol, port] =
+      host === "db.localtest.me" ? ["http", 4444] : ["https", 443];
+    return `${protocol}://${host}:${port}/sql`;
+  };
+  const connectionStringUrl = new URL(connectionString);
+  neonConfig.useSecureWebSocket =
+    connectionStringUrl.hostname !== "db.localtest.me";
+  neonConfig.wsProxy = (host) =>
+    host === "db.localtest.me" ? `${host}:4444/v2` : `${host}/v2`;
+}
 import ws from "ws";
 neonConfig.webSocketConstructor = ws;
 neonConfig.poolQueryViaFetch = true;
 
-const sql = neon(process.env.DATABASE_URL!);
+const sql = neon(connectionString);
 export const db = drizzle({ client: sql });
