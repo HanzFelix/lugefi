936871ba82200be273828f48249c2ead2fc79143
Hanz Felix Tumulak <tumulakhanz@gmail.com>
2025-07-11 16:28:08 +0800

    feat: add commenting to posts

--- src/app/forum/[id]/page.tsx
+++ src/app/forum/[id]/page.tsx
@@ -1,13 +1,15 @@
+import { auth } from "@/auth";
 import Comments from "@/components/content/Comments";
 import CommentsSkeleton from "@/components/content/CommentsSkeleton";
 import { Button } from "@/components/ui/button";
 import { db } from "@/db/database";
-import { post } from "@/db/schema/forum";
+import { comment, post } from "@/db/schema/forum";
 import { profile } from "@/db/schema/profile";
 import { RiChatNewFill } from "@remixicon/react";
-import { eq } from "drizzle-orm";
+import { eq, sql } from "drizzle-orm";
 import Image from "next/image";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 import { Suspense } from "react";
 
 async function getPost(id: number) {
@@ -30,8 +32,40 @@ export default async function ForumPost({
 }: {
   params: Promise<{ id: number }>;
 }) {
+  const session = await auth();
+
   const { id } = await params;
   const [p] = await getPost(id);
+
+  async function createComment(formData: FormData) {
+    "use server";
+
+    if (!session?.user?.id) throw new Error("no user");
+
+    const profileId = db.$with("existing_profile").as(
+      db
+        .select({
+          id: profile.id,
+        })
+        .from(profile)
+        .where(eq(profile.user_id, session.user.id))
+        .limit(1),
+    );
+
+    const [{ insertedId: commentId }] = await db
+      .with(profileId)
+      .insert(comment)
+      .values({
+        text: formData.get("comment") as string,
+        posted_at: id,
+        posted_by: sql`(select id from existing_profile)`,
+      })
+      .returning({ insertedId: post.id });
+
+    if (!commentId) throw new Error("Failed to add comment");
+
+    redirect(`/forum/${id}`);
+  }
   return (
     <div className="container mx-auto flex flex-col gap-4 px-4 md:flex-row">
       <div className="flex flex-1 flex-col gap-4">
@@ -71,21 +105,25 @@ export default async function ForumPost({
 
           <Suspense fallback={<CommentsSkeleton />}>
             <Comments postId={id} />
-            <div className="flex items-start gap-2">
-              <div className="mt-1">
-                <RiChatNewFill
-                  size={24}
-                  className="text-cmono-50 ml-auto w-8"
+            <form action={createComment}>
+              <div className="flex items-start gap-2">
+                <div className="mt-1">
+                  <RiChatNewFill
+                    size={24}
+                    className="text-cmono-50 ml-auto w-8"
+                  />
+                </div>
+                <textarea
+                  placeholder="Add a comment..."
+                  name="comment"
+                  required
+                  className="border-cpurple focus:border-cyellow text-cmono-75 focus:bg-cmono-25 placeholder:text-cmono-50 flex-1 border-l-2 py-1 pl-2 text-sm focus:outline-0"
                 />
               </div>
-              <textarea
-                placeholder="Add a comment..."
-                className="border-cpurple focus:border-cyellow text-cmono-75 focus:bg-cmono-25 placeholder:text-cmono-50 flex-1 border-l-2 py-1 pl-2 text-sm focus:outline-0"
-              />
-            </div>
-            <div className="mt-2 flex justify-end">
-              <Button disabled>Post</Button>
-            </div>
+              <div className="mt-2 flex justify-end">
+                <Button type="submit">Post</Button>
+              </div>
+            </form>
           </Suspense>
         </div>
       </div>
