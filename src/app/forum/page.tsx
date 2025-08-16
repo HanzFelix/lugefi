4305abf3d43867e60969602495dd910287caf38f
Hanz Felix Tumulak <tumulakhanz@gmail.com>
2025-08-16 13:23:57 +0800

    feat: update home page

--- src/app/forum/page.tsx
+++ src/app/forum/page.tsx
@@ -25,22 +25,24 @@ export default async function ForumPage({
           <Posts params={{ q: query, p: currentPage }} />
         </Suspense>
       </div>
-      <div className="flex w-full flex-col gap-4 md:w-1/3 lg:w-1/4">
-        <div className="border-cmono-50 text-cmono-50 w-full border-y px-2">
-          <span>Quick Links</span>
-        </div>
+      <div className="flex w-full flex-col gap-6 md:w-1/3 lg:w-1/4">
         {session && (
-          <Link
-            href={"/forum/new"}
-            className="hover:bg-cmono-25 group flex w-full"
-          >
-            <span className="group-hover:text-cyellow group-active:text-cblue group-active:border-cblue border-cblue group-hover:border-cyellow my-2 basis-full border-l-2 px-1.5">
-              Publish a post
-            </span>
-            <div className="text-cmono-50 group-active:text-cblue group-active:bg-cmono-25 group-hover:text-cyellow p-2">
-              <RiQuillPenFill />
+          <div className="flex flex-col">
+            <div className="border-cmono-50 text-cmono-50 mb-2 w-full border-y px-2">
+              <span>Quick Links</span>
             </div>
-          </Link>
+            <Link
+              href={"/forum/new"}
+              className="hover:bg-cmono-25 group flex w-full"
+            >
+              <span className="group-hover:text-cyellow group-active:text-cblue group-active:border-cblue border-cblue group-hover:border-cyellow my-2 basis-full border-l-2 px-1.5">
+                Publish a post
+              </span>
+              <div className="text-cmono-50 group-active:text-cblue group-active:bg-cmono-25 group-hover:text-cyellow p-2">
+                <RiQuillPenFill />
+              </div>
+            </Link>
+          </div>
         )}
         <form className="flex flex-col gap-4" action="/forum">
           <p className="border-cmono-50 text-cmono-50 w-full border-y px-2">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,58 +1,72 @@
 import { auth, signIn, signOut } from "@/auth";
-import { Button } from "@/components/ui/button";
-import { RiNewspaperFill } from "@remixicon/react";
+import {
+  RiGoogleFill,
+  RiLogoutBoxLine,
+  RiNewspaperFill,
+} from "@remixicon/react";
 import Link from "next/link";
 
 export default async function Home() {
   const session = await auth();
   return (
-    <div className="mx-auto w-full max-w-sm px-4">
-      {!session ? (
-        <form
-          action={async () => {
-            "use server";
-            await signIn("google", { redirectTo: "/forum" });
-          }}
-          className="text-center"
-        >
-          <h2 className="text-2xl">Welcome to Lugefi</h2>
-          <p className="mb-4">Please sign in to participate</p>
-          <Button type="submit">Sign in with Google</Button>
-        </form>
-      ) : (
-        <form
-          action={async () => {
-            "use server";
-            await signOut();
-          }}
-          className="text-center"
-        >
-          <h2 className="mb-4 text-2xl">
-            Welcome, {session.user?.name || session.user?.email}
-          </h2>
-          <button
-            type="submit"
-            className="mt-4 inline-block rounded border p-2"
+    <div className="mx-auto flex min-h-full w-full max-w-sm flex-col justify-center px-4">
+      <h2>Welcome to Lugefi</h2>
+      <p className="text-cmono-50 mb-2 text-sm">
+        {session ? `Signed in` : "Please sign in to participate"}
+      </p>
+      <section>
+        {/*<p className="border-cmono-50 text-cmono-50 mb-2 w-full border-y px-2">
+          Explore
+        </p>*/}
+        {!session && (
+          <form
+            action={async () => {
+              "use server";
+              await signIn("google", { redirectTo: "/forum" });
+            }}
           >
-            Sign Out
-          </button>
-        </form>
-      )}
-      {session && (
-        <>
-          <p className="border-cmono-50 text-cmono-50 mt-8 mb-4 w-full border-y px-2">
-            Explore
-          </p>
-          <Link href={"/forum"} className="hover:bg-cmono-25 group flex w-full">
-            <span className="group-hover:text-cyellow group-active:text-cblue group-active:border-cblue border-cblue group-hover:border-cyellow my-2 basis-full border-l-2 px-1.5">
-              Proceed to forum
-            </span>
-            <div className="text-cmono-50 group-active:text-cblue group-active:bg-cmono-25 group-hover:text-cyellow p-2">
-              <RiNewspaperFill />
-            </div>
-          </Link>
-        </>
-      )}
+            <button
+              type="submit"
+              className="hover:bg-cmono-25 group flex w-full hover:cursor-pointer"
+            >
+              <span className="group-hover:text-cyellow group-active:text-cblue group-active:border-cblue border-cblue group-hover:border-cyellow my-2 basis-full border-l-2 px-1.5 text-start">
+                Sign in with Google
+              </span>
+              <div className="text-cmono-50 group-active:text-cblue group-active:bg-cmono-25 group-hover:text-cyellow p-2">
+                <RiGoogleFill />
+              </div>
+            </button>
+          </form>
+        )}
+        <Link href={"/forum"} className="hover:bg-cmono-25 group flex w-full">
+          <span className="group-hover:text-cyellow group-active:text-cblue group-active:border-cblue border-cblue group-hover:border-cyellow my-2 basis-full border-l-2 px-1.5">
+            Proceed to forum
+          </span>
+          <div className="text-cmono-50 group-active:text-cblue group-active:bg-cmono-25 group-hover:text-cyellow p-2">
+            <RiNewspaperFill />
+          </div>
+        </Link>
+        {session && (
+          <form
+            action={async () => {
+              "use server";
+              await signOut();
+            }}
+          >
+            <button
+              type="submit"
+              className="hover:bg-cmono-25 group flex w-full hover:cursor-pointer"
+            >
+              <span className="group-hover:text-cyellow group-active:text-cblue group-active:border-cblue border-cblue group-hover:border-cyellow my-2 basis-full border-l-2 px-1.5 text-start">
+                Sign out
+              </span>
+              <div className="text-cmono-50 group-active:text-cblue group-active:bg-cmono-25 group-hover:text-cyellow p-2">
+                <RiLogoutBoxLine />
+              </div>
+            </button>
+          </form>
+        )}
+      </section>
     </div>
   );
 }
